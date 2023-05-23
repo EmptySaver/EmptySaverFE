1e0a6e84e22bc087b856d719e55b64c2e8111f58
namssaeng <ncm.bizac@gmail.com>
2023-05-23 21:41:32 +0900

    댓글 스크롤 문제, 키보드 간섭 해결

--- lib/screen/each_post_screen.dart
+++ lib/screen/each_post_screen.dart
@@ -47,6 +47,10 @@ class _EachPostScreenState extends ConsumerState<EachPostScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      // bottomSheet: Container(
+      //   height: 40,
+      //   color: Colors.amber,
+      // ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
@@ -62,141 +66,76 @@ class _EachPostScreenState extends ConsumerState<EachPostScreen> {
                   var commentList = snapshot.data!['comments'] as List;
                   return (widget.mode == 'read')
                       ? Expanded(
-                          child: Stack(
-                            children: [
-                              SingleChildScrollView(
-                                child: Column(
-                                  children: [
-                                    Text('${snapshot.data!['title']}'),
-                                    Text('${snapshot.data!['content']}'),
-                                    const Divider(thickness: 1.5),
-                                    (commentList != null)
-                                        ? SizedBox(
-                                            height: 630,
-                                            child: ListView.builder(
-                                              itemCount: commentList.length,
-                                              itemBuilder: (context, index) {
-                                                var parentComment =
-                                                    commentList[index]
-                                                        ['parent'];
-                                                var childCommentList =
-                                                    commentList[index]
-                                                        ['childList'];
-                                                return Container(
-                                                  padding:
-                                                      const EdgeInsets.all(5),
-                                                  height: 100,
-                                                  decoration:
-                                                      const BoxDecoration(
-                                                          border: Border(
-                                                              bottom: BorderSide(
-                                                                  width: 1,
-                                                                  color: Colors
-                                                                      .grey))),
-                                                  child: Column(
-                                                    crossAxisAlignment:
-                                                        CrossAxisAlignment
-                                                            .start,
-                                                    children: [
-                                                      Row(
-                                                        mainAxisAlignment:
-                                                            MainAxisAlignment
-                                                                .spaceBetween,
-                                                        children: [
-                                                          const Text('아이콘,이름'),
-                                                          ButtonBar(
-                                                            children: [
-                                                              IconButton(
-                                                                  padding:
-                                                                      EdgeInsets
-                                                                          .zero,
-                                                                  constraints:
-                                                                      const BoxConstraints(),
-                                                                  onPressed:
-                                                                      () {},
-                                                                  icon: const Icon(
-                                                                      Icons
-                                                                          .add_comment_rounded)),
-                                                              IconButton(
-                                                                  padding:
-                                                                      EdgeInsets
-                                                                          .zero,
-                                                                  constraints:
-                                                                      const BoxConstraints(),
-                                                                  onPressed:
-                                                                      () {},
-                                                                  icon: const Icon(
-                                                                      Icons
-                                                                          .more_vert_rounded)),
-                                                            ],
-                                                          )
-                                                        ],
-                                                      ),
-                                                      Text(parentComment[
-                                                          'text']),
-                                                      const SizedBox(
-                                                        height: 5,
-                                                      ),
-                                                      Text(
-                                                          '${parentComment['dateTime'].toString().substring(0, 10)} ${parentComment['dateTime'].toString().substring(11, 19)}'),
-                                                    ],
-                                                  ),
-                                                );
-                                              },
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                Text('${snapshot.data!['title']}'),
+                                Text('${snapshot.data!['content']}'),
+                                const Divider(thickness: 1.5),
+                                (commentList.isNotEmpty)
+                                    ? ListView.builder(
+                                        shrinkWrap: true,
+                                        primary: false,
+                                        itemCount: commentList.length,
+                                        itemBuilder: (context, index) {
+                                          var parentComment =
+                                              commentList[index]['parent'];
+                                          var childCommentList =
+                                              commentList[index]['childList'];
+                                          return Container(
+                                            padding: const EdgeInsets.all(5),
+                                            height: 100,
+                                            decoration: const BoxDecoration(
+                                                border: Border(
+                                                    bottom: BorderSide(
+                                                        width: 1,
+                                                        color: Colors.grey))),
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    const Text('아이콘,이름'),
+                                                    ButtonBar(
+                                                      children: [
+                                                        IconButton(
+                                                            padding:
+                                                                EdgeInsets.zero,
+                                                            constraints:
+                                                                const BoxConstraints(),
+                                                            onPressed: () {},
+                                                            icon: const Icon(Icons
+                                                                .add_comment_rounded)),
+                                                        IconButton(
+                                                            padding:
+                                                                EdgeInsets.zero,
+                                                            constraints:
+                                                                const BoxConstraints(),
+                                                            onPressed: () {},
+                                                            icon: const Icon(Icons
+                                                                .more_vert_rounded)),
+                                                      ],
+                                                    )
+                                                  ],
+                                                ),
+                                                Text(parentComment['text']),
+                                                const SizedBox(
+                                                  height: 5,
+                                                ),
+                                                Text(
+                                                    '${parentComment['dateTime'].toString().substring(0, 10)} ${parentComment['dateTime'].toString().substring(11, 19)}'),
+                                              ],
                                             ),
-                                          )
-                                        : const Text('댓글 없음'),
-                                  ],
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.bottomCenter,
-                                child: SizedBox(
-                                  height: 50,
-                                  width: double.infinity,
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                        child: TextField(
-                                          decoration: const InputDecoration(
-                                              hintText: '댓글을 입력하세요'),
-                                          controller: commentTec,
-                                        ),
-                                      ),
-                                      IconButton(
-                                          onPressed: () async {
-                                            var url = Uri.http(baseUri,
-                                                '/board/addPostComment');
-                                            var response = await http.post(url,
-                                                headers: {
-                                                  'authorization':
-                                                      'Bearer $jwtToken',
-                                                  'Content-Type':
-                                                      'application/json; charset=UTF-8'
-                                                },
-                                                body: jsonEncode({
-                                                  'groupId': widget.groupId,
-                                                  'parentCommentId': -1,
-                                                  'postId': widget.postId,
-                                                  'text': commentTec.text,
-                                                }));
-                                            if (response.statusCode == 200) {
-                                              print('댓글');
-                                              commentTec.clear();
-                                              setState(() {
-                                                postDetailFuture = getPost();
-                                              });
-                                            } else {
-                                              print(utf8
-                                                  .decode(response.bodyBytes));
-                                            }
-                                          },
-                                          icon: const Icon(Icons.send_rounded))
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
+                                          );
+                                        },
+                                      )
+                                    : const Text('댓글 없음'),
+                              ],
+                            ),
                           ),
                         )
                       : Column(
@@ -242,7 +181,51 @@ class _EachPostScreenState extends ConsumerState<EachPostScreen> {
                   );
                 }
               },
-            )
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration:
+                            const InputDecoration(hintText: '댓글을 입력하세요'),
+                        controller: commentTec,
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () async {
+                          var url = Uri.http(baseUri, '/board/addPostComment');
+                          var response = await http.post(url,
+                              headers: {
+                                'authorization': 'Bearer $jwtToken',
+                                'Content-Type':
+                                    'application/json; charset=UTF-8'
+                              },
+                              body: jsonEncode({
+                                'groupId': widget.groupId,
+                                'parentCommentId': -1,
+                                'postId': widget.postId,
+                                'text': commentTec.text,
+                              }));
+                          if (response.statusCode == 200) {
+                            print('댓글');
+                            commentTec.clear();
+                            setState(() {
+                              postDetailFuture = getPost();
+                            });
+                          } else {
+                            print(utf8.decode(response.bodyBytes));
+                          }
+                        },
+                        icon: const Icon(Icons.send_rounded))
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
